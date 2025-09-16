9d64df363bd7ce65bbc3f429d6124a61ce7b2c8a
ariwen <yinglan.wen@gmail.com>
2025-09-16 16:48:47 +1000

    movement
    able to make it move after clicked. still need to fix movement to go down and up as well as sync audio

--- poem.js
+++ poem.js
@@ -4,17 +4,33 @@ console.log(line1)
 const synth = new Tone.Synth().toDestination();
 const now = Tone.now();
 
-let speed = 5;
-let bouncing = false; 
-let dragging = false; 
+let position = 50;
+let direction = 1;
+let acceleration = 0.1;
+const bottom = 550; 
 
-function move () {
-    requestAnimationFrame(move); 
-}
+
+
+line1.addEventListener("click", function animate () {
+    position += direction *5
+    direction -= acceleration;
+    line1.style.top = position + 'px';
+    if (position => 50) {
+        direction = 1;
+    if (position >= bottom) {
+        direction = -1; 
+    }
+    
+    requestAnimationFrame(animate);
+}); 
+    
+
+
 
 
 line1.addEventListener("click", async () => {
     await Tone.start();
     console.log("started");
-    synth.triggerAttackRelease("C4", "10n", now + 0.5);
+    synth.triggerAttackRelease("C4", "8n", now + 4);
 })
+
